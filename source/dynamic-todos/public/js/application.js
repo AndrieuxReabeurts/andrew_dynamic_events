21bbc186dae2e1fb27a4142a5c86ac1368f29592
Andrew <jdrewsdc@gmail.com>
2014-04-14 01:52:15 -0700

    what works on sunday night

--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+
+    $('form').on('submit', addTodo)
+    // {
+    //   event.preventDefault();
+    //   console.log(event.target) // => <form action="/" method="post">
+    //   addData = $(this);
+    //   console.log(addData);
+    // })
+
+    $('todo_template').on('click', '.complete', completeTodo)
+    $('todo_template').on('click', '.delete', deleteTodo)
+
+
   }
 
   //Create functions to add, remove and complete todos
@@ -17,6 +30,33 @@ $(document).ready(function() {
     $todo.find('h2').text(todoName);
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
+  }
+
+  function addTodo(event) {
+    event.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      data: $(this),
+      url: '/add_todo',
+      success: function(data){
+        //onsole.log(data)
+        console.log(buildTodo(data))
+        $('.todo_list').append(buildTodo(data))
+
+      }
+
+    })
+    event
+
+  }
+
+  function completeTodo() {
+
+  }
+
+  function deleteTodo() {
+
   }
 
 
